03497419e0a7700341420fd8656c26c32363d9b2
liruidesysu <li_read@outlook.com>
2018-12-06 12:32:57 +0800

    首页页面精细化
    更换UI图标，添加底部导航栏

---
+++ OrderFood/app/src/main/java/com/example/administrator/orderfood/FirstFragment.java
@@ -0,0 +1,30 @@
+package com.example.administrator.orderfood;
+
+import android.annotation.SuppressLint;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+@SuppressLint("ValidFragment")
+public class FirstFragment extends Fragment {
+    private String context;
+    private TextView mTextView;
+
+    public FirstFragment(String context) {
+        this.context = context;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.first_fragment, container, false);
+        mTextView = (TextView) view.findViewById(R.id.txt_content);
+        //mTextView = (TextView)getActivity().findViewById(R.id.txt_content);
+        mTextView.setText(context);
+        return view;
+    }
+}
--- OrderFood/app/src/main/java/com/example/administrator/orderfood/MainActivity.java
+++ OrderFood/app/src/main/java/com/example/administrator/orderfood/MainActivity.java
@@ -1,13 +1,108 @@
 package com.example.administrator.orderfood;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private TextView topBar;
+    private TextView tabHome;
+    private TextView tabOrder;
+    private TextView tabUser;
+
+    private FrameLayout ly_content;
+
+    private FirstFragment f1,f2,f3;
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        bindView();
+
+    }
+
+    //UI组件初始化与事件绑定
+    private void bindView() {
+        topBar = (TextView)this.findViewById(R.id.txt_top);
+        tabHome = (TextView)this.findViewById(R.id.txt_home);
+        tabOrder = (TextView)this.findViewById(R.id.txt_order);
+        tabUser = (TextView)this.findViewById(R.id.txt_user);
+        ly_content = (FrameLayout) findViewById(R.id.fragment_container);
+
+        tabHome.setOnClickListener(this);
+        tabUser.setOnClickListener(this);
+        tabOrder.setOnClickListener(this);
+
+    }
+
+    //重置所有文本的选中状态
+    public void selected(){
+        tabHome.setSelected(false);
+        tabOrder.setSelected(false);
+        tabUser.setSelected(false);
+    }
+
+    //隐藏所有Fragment
+    public void hideAllFragment(FragmentTransaction transaction){
+        if(f1!=null){
+            transaction.hide(f1);
+        }
+        if(f2!=null){
+            transaction.hide(f2);
+        }
+        if(f3!=null){
+            transaction.hide(f3);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        hideAllFragment(transaction);
+        switch(v.getId()){
+            case R.id.txt_home:
+                selected();
+                tabHome.setSelected(true);
+                if(f1==null){
+                    f1 = new FirstFragment("第一个Fragment");
+                    transaction.add(R.id.fragment_container,f1);
+                }else{
+                    transaction.show(f1);
+                }
+                break;
+
+            case R.id.txt_order:
+                selected();
+                tabOrder.setSelected(true);
+                if(f2==null){
+                    f2 = new FirstFragment("第二个Fragment");
+                    transaction.add(R.id.fragment_container,f2);
+                }else{
+                    transaction.show(f2);
+                }
+                break;
+
+            case R.id.txt_user:
+                selected();
+                tabUser.setSelected(true);
+                if(f3==null){
+                    f3 = new FirstFragment("第三个Fragment");
+                    transaction.add(R.id.fragment_container,f3);
+                }else{
+                    transaction.show(f3);
+                }
+                break;
+        }
+
+        transaction.commit();
     }
 }
